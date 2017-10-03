69663cf0d98c96878d8082fe81939b7ac074e474
Daniel Vahla <vahla.daniel@gmail.com>
2017-10-03 16:47:12 +0300

    First day sketch

---
+++ app/src/main/java/daniev/androidchat/Sender.java
@@ -0,0 +1,47 @@
+package daniev.androidchat;
+
+import android.util.Log;
+
+import java.io.PrintStream;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by danie on 3.10.2017.
+ */
+
+public class Sender implements Runnable {
+
+    private ArrayBlockingQueue<String> messageQueue;
+    private PrintStream output;
+
+    public Sender(PrintStream outputStream) {
+        this.output = outputStream;
+        this.messageQueue = new ArrayBlockingQueue<>(10);
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+
+            try {
+                String newestMessage = this.messageQueue.take();
+                sendMSG(newestMessage);
+
+            } catch (InterruptedException i) {
+                Log.d(TAG, "ERROR WHILE ACCESSING MESSAGE BLOCKING QUEUE");
+                i.printStackTrace();
+            }
+        }
+    }
+
+    public void addMessage(String msg) {
+        this.messageQueue.add(msg);
+    }
+
+    public void sendMSG(String msg) {
+        String message = msg;
+        output.println(message);
+    }
+}
